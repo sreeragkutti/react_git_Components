5c03612924f357c464e54966aed2f17ec7522289
Sreerag Kuttipurath <sreerag801@outlook.com>
2019-01-10 16:06:13 +0530

    some usefull git commands added

--- src/index.tsx
+++ src/index.tsx
@@ -10,3 +10,20 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+// Some usefull git commands
+// ---------------------------------------------------
+// git add 
+// git commit -m "description of your commit" 
+// git push origin MyNewBranch 
+// git checkout -b MyNewBranch 
+// git add -p 
+//  ---------------------------------------------------
+// 1. git clone git_repo_url 
+// 2. git pull origin master 
+// 3. git checkout -b your_new_branch 
+// 4. git add --all
+// git commit -am "describe your changes" 
+// 5. git push origin your_new_branch 
+// -----------------------------------------------------
